79f66a60818d62dcedd87a40fc991b89e1771d21
LaylConway <laylconway@live.com>
2014-06-30 08:43:45 +0200

    Added incomplete Pbrain and Brainfork extensions.

--- BrainFry/Commands/CommandPresets.cs
+++ BrainFry/Commands/CommandPresets.cs
@@ -1,25 +1,71 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace BrainFry.Commands
 {
 	public static class CommandPresets
 	{
+		private static IDictionary<char, ICommand> Merge(params IDictionary<char, ICommand>[] dictionaries)
+		{
+			return dictionaries.SelectMany(d => d).ToDictionary(d => d.Key, d => d.Value);
+		}
+
+		public static IDictionary<char, ICommand> PbrainExtension
+		{
+			get
+			{
+				return new Dictionary<char, ICommand>
+				{
+					{'(', new IncrementCommand()},
+					{')', new DecrementCommand()},
+				};
+			}
+		}
+
+		public static IDictionary<char, ICommand> BrainforkExtension
+		{
+			get
+			{
+				return new Dictionary<char, ICommand>
+				{
+				};
+			}
+		}
+
+		public static IDictionary<char, ICommand> Brainfuck
+		{
+			get
+			{
+				return new Dictionary<char, ICommand>
+				{
+					{'+', new IncrementCommand()},
+					{'-', new DecrementCommand()},
+
+					{',', new InputCommand()},
+					{'.', new OutputCommand()},
+				
+					{'>', new PointerIncrementCommand()},
+					{'<', new PointerDecrementCommand()},
+				
+					{'[', new LoopOpenCommand()},
+					{']', new LoopCloseCommand()}
+				};
+			}
+		}
+
 		public static IDictionary<char, ICommand> Default
 		{
-			get { return new Dictionary<char, ICommand>
-			{
-				{'+', new IncrementCommand()},
-				{'-', new DecrementCommand()},
+			get { return Merge(Brainfuck, PbrainExtension, BrainforkExtension); }
+		}
 
-				{',', new InputCommand()},
-				{'.', new OutputCommand()},
-				
-				{'>', new PointerIncrementCommand()},
-				{'<', new PointerDecrementCommand()},
-				
-				{'[', new LoopOpenCommand()},
-				{']', new LoopCloseCommand()}
-			};}
+		public static IDictionary<char, ICommand> Pbrain
+		{
+			get { return Merge(Brainfuck, PbrainExtension); }
+		}
+
+		public static IDictionary<char, ICommand> Brainfork
+		{
+			get { return Merge(Brainfuck, BrainforkExtension); }
 		}
 	}
 }
