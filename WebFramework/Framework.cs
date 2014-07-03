35b53937ae414f6cf84a91038b2da35373cd75cc
Layl Conway <laylconway@live.com>
2014-07-03 04:11:25 +0200

    Minor improvement.

--- WebFramework/Framework.cs
+++ WebFramework/Framework.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Text;
 
@@ -12,11 +13,12 @@ namespace WebFramework
 			// Get the request metadata
 			var request = context.Request;
 			var response = context.Response;
+			var targetDirectory = new DirectoryInfo("." + request.Url.LocalPath);
 
-			Trace.TraceInformation("{0} {1} {2}", request.HttpMethod, DateTime.Now.ToString("O"), request.Url.LocalPath);
+			Trace.TraceInformation("{0} {1}: {2}", DateTime.Now.ToString("O"), request.HttpMethod, targetDirectory);
 
 			// Construct a response
-			var buffer = Encoding.UTF8.GetBytes("<html><body>Hello world!</body></html>");
+			var buffer = Encoding.UTF8.GetBytes(string.Format("<html><body>Hello, {0}!</body></html>", targetDirectory.FullName));
 
 			// Get a response stream and write the response to it
 			var output = response.OutputStream;
